8a72ddb84cb1ec9330d20d07243d766632fe2c90
System Administrator <root@Ammars-MacBook-Pro.local>
2022-03-09 14:39:11 +1100

    users.php updated

--- users.php
+++ users.php
@@ -13,13 +13,43 @@ if ($mysqli -> connect_errno) {
   echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
   exit();
 }else{
-	//echo "success";
+	echo "success";
 }
 
 
+//  Parsing CSV file to PHP
 
 
 
+if (($open = fopen("users.csv", "r+")) !== FALSE) 
+{
+  
+   $data = fgetcsv($open, 1000, ",");  
+
+
+
+    $row = 1;
+   	while (($data = fgetcsv($open, 1000, ",")) !== FALSE) 
+   	{
+        $num = count($data);
+
+        //get data by line
+        $row++;
+
+        for ($c=0; $c < $num; $c++) 
+        {
+            
+        	echo $data[$c];
+           
+
+        
+
+
+        }
+
+    }
+
+}
 
 
 
